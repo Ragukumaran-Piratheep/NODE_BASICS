771cd8d50f39e7b20181716cd3c247f8b2c48b72
Ragukumaran Piratheep <rpiratheep2002@gmail.com>
2023-11-25 23:06:13 +0530

    added

--- 001_local_module/maths.js
+++ 001_local_module/maths.js
@@ -1,7 +1,7 @@
 // const add = (a, b) => a + b;
 // const sub = (a, b) => a - b;
-// const mul = (a, b) => a * b;
-// const div = (a, b) => a / b;
+ const mul = (a, b) => a * b;
+const div = (a, b) => a / b;
 
 exports.add = (a, b) => a + b;
 exports.sub = (a, b) => a - b;
