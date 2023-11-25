887d42b6cb4814685bda01400605906ffaf3fcc3
Ragukumaran Piratheep <rpiratheep2002@gmail.com>
2023-11-25 23:05:40 +0530

    added

--- 001_local_module/sample.js
+++ 001_local_module/sample.js
@@ -1,11 +1,11 @@
-// const a = 10;
-// const b = 20;
-// let c = 30;
-// console.log(c);
+const a = 10;
+const b = 20;
+let c = 30;
+console.log(c);
 
- const math = require("./maths");
- console.log(math.add(25, 35));
- console.log(math.sub(45,35));
+const math = require("./maths");
+console.log(math.add(25, 35));
+console.log(math.sub(45, 35));
 
 const { add, sub, mul, div } = require("./maths");
 console.log(add(26, 87));
